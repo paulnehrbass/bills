55010a1f2eb338fd2ff626a3d816496e34052759
paul.nehrbass <paul.nehrbass@web.de>
2026-01-04 12:57:38 +0100

    Menu-Limks angepasst auf cakePHP

---
+++ stuff/my.twig.php
@@ -0,0 +1,36 @@
+<form>
+    <div class="row mb-4">
+        <label for="horizontal-firstname-input" class="col-sm-3 col-form-label">First name</label>
+        <div class="col-sm-9">
+            <input type="text" class="form-control" id="horizontal-firstname-input" placeholder="Enter Your ">
+        </div>
+    </div>
+    <div class="row mb-4">
+        <label for="horizontal-email-input" class="col-sm-3 col-form-label">Email</label>
+        <div class="col-sm-9">
+            <input type="email" class="form-control" id="horizontal-email-input" placeholder="Enter Your Email ID">
+        </div>
+    </div>
+    <div class="row mb-4">
+        <label for="horizontal-password-input" class="col-sm-3 col-form-label">Password</label>
+        <div class="col-sm-9">
+            <input type="password" class="form-control" id="horizontal-password-input" placeholder="Enter Your Password">
+        </div>
+    </div>
+
+    <div class="row justify-content-end">
+        <div class="col-sm-9">
+
+            <div class="form-check mb-4">
+                <input class="form-check-input" type="checkbox" id="horizontalLayout-Check">
+                <label class="form-check-label" for="horizontalLayout-Check">
+                    Remember me
+                </label>
+            </div>
+
+            <div>
+                <button type="submit" class="btn btn-primary w-md">Submit</button>
+            </div>
+        </div>
+    </div>
+</form>
